ca11f8538fce1a34f4cfd4a0eeb918ccd62443a9
Yuhan Wang <yh-w24@mails.tsinghua.edu.cn>
2026-03-04 13:07:50 +0000

    test: add ResNet

--- tools/ventus_regression_profile.py
+++ tools/ventus_regression_profile.py
@@ -102,7 +102,7 @@ def _ventus_testcases(ventus_root: Path) -> list[TestCase]:
         TestCase(name="alexnet", path=ventus_testcase_dir / "_get_case/AlexNet", cmd=["./AlexNet.out"], timeout_s=240),
         TestCase(name="lenet5", path=ventus_testcase_dir / "_get_case/LeNet5", cmd=["./LeNet5.out"], timeout_s=240),
         TestCase(name="lenet5_simple", path=ventus_testcase_dir / "_get_case/LeNet5-simple", cmd=["./LeNet5-simple.out"], timeout_s=240),
-        # TestCase(name="resnet18", path=ventus_testcase_dir / "_get_case/ResNet", cmd=["./ResNet18.out"], timeout_s=600),
+        TestCase(name="resnet18", path=ventus_testcase_dir / "_get_case/ResNet", cmd=["./ResNet18.out"], timeout_s=600),
     ]
 
 
